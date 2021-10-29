5c856cbb47d482dba88e456e0ad1d9a2dbc6d08a
hulefei <hulefei29@gmail.com>
2021-10-29 21:02:13 +0800

    【add】 添加track

--- Plugins/CustomAsset/Source/CustomAssetEditor/Private/KTimeline/SKTimelineTrackNode.cpp
+++ Plugins/CustomAsset/Source/CustomAssetEditor/Private/KTimeline/SKTimelineTrackNode.cpp
@@ -4,17 +4,79 @@
 #include "SKTimelineTrackNode.h"
 
 #include "SlateOptMacros.h"
+#include "Styles/CustomAssetEditorStyle.h"
 
 BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
 
 void SKTimelineTrackNode::Construct(const FArguments& InArgs)
 {
+	SlateFontInfo = FEditorStyle::GetFontStyle("PropertyWindow.NormalFont");
+	SlateFontInfo.Size = 12;
+	NodeBrush = FCustomAssetEditorStyle::GetBrush("CustomAssetEditor.Node");
+	HighlightNodeBrush = FCustomAssetEditorStyle::GetBrush("CustomAssetEditor.NodeHighlight");
+	TextBackgroundBrush = FCustomAssetEditorStyle::GetBrush("CustomAssetEditor.NodeText");
+
+	DragType = MoveNone;
+	MaxLength = 100.0f;
+
+	
 	/*
 	ChildSlot
 	[
 		// Populate the widget
 	];
 	*/
+}
+
+FReply SKTimelineTrackNode::OnMouseButtonDown(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent)
+{
+	const bool IsLMBDown = MouseEvent.IsMouseButtonDown(EKeys::LeftMouseButton);
+	const bool IsRMBDown = MouseEvent.IsMouseButtonDown(EKeys::RightMouseButton);
+	if (IsLMBDown)
+	{
+		//TODO:判断类型
+		DragType = MoveCenter;
+		return FReply::Handled().DetectDrag(SharedThis(this), EKeys::LeftMouseButton);
+	}
+
+	return FReply::Unhandled();
+}
+
+FReply SKTimelineTrackNode::OnMouseButtonUp(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent)
+{
+	UE_LOG(LogTemp, Log, TEXT("SKSkillSimpleTimelineTrackNode::OnMouseButtonUp"));
+	if (DragType != EKSkillDragType::MoveNone)
+	{
+		DragType = EKSkillDragType::MoveNone;
+		return FReply::Handled().ReleaseMouseCapture();
+	}
+	return FReply::Unhandled();
+}
+
+FReply SKTimelineTrackNode::OnMouseMove(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent)
+{
+	if (!HasMouseCapture())
+	{
+		return FReply::Unhandled();
+	}
+
+	const FVector2D& ParentWidgetSize = MyGeometry.GetLocalSize();
+	const FVector2D MousePos = MyGeometry.AbsoluteToLocal(MouseEvent.GetScreenSpacePosition());
+	float MouseTimePosition = MousePos.X / ParentWidgetSize.X * MaxLength;
+	MouseTimePosition = FMath::Clamp(MouseTimePosition, 0.0f, MaxLength);
+	Duration = MouseTimePosition;
+
+	return FReply::Unhandled();
+}
+
+FReply SKTimelineTrackNode::OnDragDetected(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent)
+{
+	return FReply::Handled().CaptureMouse(SharedThis(this));
+}
+
+FCursorReply SKTimelineTrackNode::OnCursorQuery(const FGeometry& MyGeometry, const FPointerEvent& CursorEvent) const
+{
+	return FCursorReply::Cursor(EMouseCursor::GrabHand);
 }
 
 FVector2D SKTimelineTrackNode::ComputeDesiredSize(float Scale) const
@@ -24,14 +86,47 @@ FVector2D SKTimelineTrackNode::ComputeDesiredSize(float Scale) const
 	// {
 	// 	return TrackNode.Get()->ComputeDesiredSize(Scale);
 	// }
-	return FVector2D(100.0f, 30.0f);
+	return FVector2D(100.0f, 26.0f);
 }
 
 int32 SKTimelineTrackNode::OnPaint(const FPaintArgs& Args, const FGeometry& AllottedGeometry,
 	const FSlateRect& MyClippingRect, FSlateWindowElementList& OutDrawElements, int32 LayerId,
 	const FWidgetStyle& InWidgetStyle, bool bParentEnabled) const
 {
-	return 0;
+	const TSharedRef<FSlateFontMeasure> FontMeasureService = FSlateApplication::Get().GetRenderer()->
+		GetFontMeasureService();
+	const FVector2D& ParentWidgetSize = AllottedGeometry.GetLocalSize();
+	const FVector2D DependencyIconSize(12.0f, 12.0f);
+	const FVector2D TextBackgroundPadding(2.0f, 0.0f);
+
+	float SegmentStartTime = 0.0f;
+	// MaxLength = FMath::Clamp(MaxLength, 0.1f, 10.00f);
+
+	float ActionSizeRatio = Duration == 0 ? MaxLength * 0.05f : Duration / MaxLength;
+	ActionSizeRatio = FMath::Clamp(ActionSizeRatio, 0.05f, 1.0f);
+	const float Size = ParentWidgetSize.X * ActionSizeRatio;
+
+	int32 CurrentLayer = LayerId;
+	const FVector2D NodeSize(Size, ParentWidgetSize.Y);
+	// const FVector2D NodeSize(15, 15);
+	const FVector2D NodeTranslation(0, 0.0f);
+	const FSlateLayoutTransform NodeOffset(NodeTranslation);
+
+	// Node
+	FSlateDrawElement::MakeBox(OutDrawElements,
+							CurrentLayer++,
+							AllottedGeometry.ToPaintGeometry(NodeSize, NodeOffset),
+							NodeBrush,
+							ESlateDrawEffect::None,
+							GetNodeColor());
+	
+	return SCompoundWidget::OnPaint(Args, AllottedGeometry, MyClippingRect, OutDrawElements, LayerId, InWidgetStyle,
+									bParentEnabled);
+}
+
+FLinearColor SKTimelineTrackNode::GetNodeColor() const
+{
+	return FLinearColor::Yellow;
 }
 
 END_SLATE_FUNCTION_BUILD_OPTIMIZATION
--- Plugins/CustomAsset/Source/CustomAssetEditor/Private/KTimeline/SKTimelineTrackNode.h
+++ Plugins/CustomAsset/Source/CustomAssetEditor/Private/KTimeline/SKTimelineTrackNode.h
@@ -5,9 +5,15 @@
 #include "CoreMinimal.h"
 #include "Widgets/SCompoundWidget.h"
 
-/**
- * 
- */
+enum EKSkillDragType
+{
+	MoveNone = 0,
+	MoveCenter,
+	MoveStart,
+	MoveEnd,
+	TotalTypes
+};
+
 class CUSTOMASSETEDITOR_API SKTimelineTrackNode : public SCompoundWidget
 {
 public:
@@ -20,7 +26,30 @@ SLATE_BEGIN_ARGS(SKTimelineTrackNode)
 	/** Constructs this widget with InArgs */
 	void Construct(const FArguments& InArgs);
 	// Rendering/Size overrides
-	virtual FVector2D ComputeDesiredSize(float Scale) const override;
+
+	// Input catching
+	virtual FReply OnMouseButtonDown(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
+	virtual FReply OnMouseButtonUp(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
+	virtual FReply OnMouseMove(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
+	//用于判断是否属于drag，在Move中可以直接使用
+	virtual FReply OnDragDetected(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
+	//返回鼠标样式
+	virtual FCursorReply OnCursorQuery(const FGeometry& MyGeometry, const FPointerEvent& CursorEvent) const override;
+
+	// Rendering/Size overrides
+	virtual FVector2D ComputeDesiredSize(float LayoutScaleMultiplier) const override;
+	//每帧draw
 	virtual int32 OnPaint(const FPaintArgs& Args, const FGeometry& AllottedGeometry, const FSlateRect& MyClippingRect,
 		FSlateWindowElementList& OutDrawElements, int32 LayerId, const FWidgetStyle& InWidgetStyle, bool bParentEnabled) const override;
+	FLinearColor GetNodeColor() const;
+
+private:
+	FSlateFontInfo SlateFontInfo;
+	const FSlateBrush* NodeBrush = nullptr;
+	const FSlateBrush* HighlightNodeBrush = nullptr;
+	const FSlateBrush* TextBackgroundBrush = nullptr;
+	/* Our current Drag operation type. */
+	EKSkillDragType DragType = EKSkillDragType::MoveNone;
+	float MaxLength = 100.0f;
+	float Duration = 50.f;
 };
